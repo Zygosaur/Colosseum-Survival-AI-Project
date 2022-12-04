5834b810b4e068d1075c3a2425cf21fa3c22955c
Alison YU <alisonyu119@gmail.com>
2022-12-04 01:08:28 -0500

    check

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -102,6 +102,7 @@ class StudentAgent(Agent):
                             best_heuristic = temp_heuristic
                             position[0] = i
                             position[1] = j
+        position = tuple(position)
         return position
     
     def get_direction(self, chess_board, my_pos, adv_pos, max_step,position):
@@ -129,86 +130,24 @@ class StudentAgent(Agent):
         # Checks the valid directions to place a barrier
         valid_dir = []
         for dir in range(4):
-            if self.check_valid_step(my_pos,tuple(position),adv_pos, chess_board, dir,max_step):
+            if self.check_valid_step(my_pos, position, adv_pos, chess_board, dir,max_step):
                 valid_dir.append(dir)
-                my_pos = tuple(position)
-                break
-
+        
+        x_diff = adv_pos[0] - position[0]
+        y_diff = adv_pos[1] - position[1]
         # Place the barrier facing the opponent
         # Randomly choose the direction of the variable if the above is unavailable
-        if adv_pos[1] > position[1] and 0 in valid_dir:
-            dir = 0
-        elif adv_pos[0] > position[0] and 1 in valid_dir:
+        if y_diff > 0 and abs(y_diff) < max_step and 1 in valid_dir:
             dir = 1
-        elif adv_pos[1] < position[1] and 2 in valid_dir:
+        elif x_diff > 0 and abs(x_diff) < max_step and 2 in valid_dir:
             dir = 2
-        elif adv_pos[0] > position[0] and 3 in valid_dir:
+        elif y_diff < 0 and abs(y_diff) < max_step and 3 in valid_dir:
             dir = 3
+        elif x_diff < 0 and abs(x_diff) < max_step and 0 in valid_dir:
+            dir = 0
         else:
             dir = random.choice(valid_dir )
         return dir
-
-    def check_endgame(self, chess_board, my_pos, adv_pos):
-        """
-        Check if the game ends and compute the current score of the agents.
-
-        Parameters
-        ----------
-        chess_board : np.ndarray
-            The current chess board.
-        my_pos : tuple
-            The position of the agent.
-        adv_pos : tuple
-            The position of the opponent.
-
-        Returns
-        -------
-        is_endgame : bool
-            Whether the game ends.
-        player_1_score : int
-            The score of player 1.
-        player_2_score : int
-            The score of player 2.
-        """
-        board_size = chess_board.shape[0]
-        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
-
-        # Union-Find
-        father = dict()
-        for r in range(board_size):
-            for c in range(board_size):
-                father[(r, c)] = (r, c)
-
-        def find(pos):
-            if father[pos] != pos:
-                father[pos] = find(father[pos])
-            return father[pos]
-
-        def union(pos1, pos2):
-            father[pos1] = pos2
-
-        for r in range(board_size):
-            for c in range(board_size):
-                for dir, move in enumerate(
-                    moves[1:3]
-                ):  # Only check down and right
-                    if chess_board[r, c, dir + 1]:
-                        continue
-                    pos_a = find((r, c))
-                    pos_b = find((r + move[0], c + move[1]))
-                    if pos_a != pos_b:
-                        union(pos_a, pos_b)
-
-        for r in range(board_size):
-            for c in range(board_size):
-                find((r, c))
-        p0_r = find(tuple(my_pos))
-        p1_r = find(tuple(adv_pos))
-        p0_score = list(father.values()).count(p0_r)
-        p1_score = list(father.values()).count(p1_r)
-        if p0_r == p1_r:
-            return False, p0_score, p1_score
-        return True, p0_score, p1_score
     
     def check_valid_step(self, start_pos, end_pos, adv_pos, chess_board, barrier_dir, max_step):
         """
@@ -255,11 +194,11 @@ class StudentAgent(Agent):
                     continue
 
                 next_pos = (cur_pos[0]+move[0], cur_pos[1]+move[1])
-                next_equals_adv = next_pos[0] == adv_pos[0] and next_pos[1] == adv_pos[1]
-                next_equals_end = next_pos[0] == end_pos[0] and next_pos[1] == end_pos[1]
-                if next_equals_adv or tuple(next_pos) in visited:
+                next_adv = next_pos[0] == adv_pos[0] and next_pos[1] == adv_pos[1]
+                next_end = next_pos[0] == end_pos[0] and next_pos[1] == end_pos[1]
+                if next_adv or tuple(next_pos) in visited:
                     continue
-                if next_equals_end:
+                if next_end:
                     is_reached = True
                     break
 
@@ -292,10 +231,6 @@ class StudentAgent(Agent):
         n = chess_board.shape[0]
         heuristic = 9999
 
-        # Determines if the game has ended
-        if self.check_endgame(chess_board, my_pos, adv_pos)[0]:
-            return -9999
-
         # Finds the Manhattan distance between the my_pos and adv_pos
         x = abs(adv_pos[0] - my_pos[0])
         y = abs(adv_pos[0] - my_pos[1])
@@ -307,14 +242,9 @@ class StudentAgent(Agent):
             if chess_board[my_pos[0],my_pos[1],dir]== True:
                 barriers_around += 1
 
-        # If there are 3 barriers around us, return 9999
-        if barriers_around ==3:   
+        # If there are 2 or more barriers around us, return 9999
+        if barriers_around >= 2:   
             return 9999  
-
-        # If there are 2 barriers around us, it isn't fatal but it's not ideal
-        # The heuristic would be slightly higher
-        if barriers_around == 2:
-            heuristic = heuristic *50
         
 
         return heuristic
